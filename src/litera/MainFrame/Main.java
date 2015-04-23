41394db3a146f5e2c170dae571a4ca1c5401d8f3
Çelik Köseoğlu <celikkoseoglu@yahoo.com>
2015-04-23 17:13:46 +0300

    New icons are here...

--- src/litera/MainFrame/Main.java
+++ src/litera/MainFrame/Main.java
@@ -52,7 +52,36 @@ public class Main extends Application
             Controller.onExit();
             System.out.println("Closed!");
         });
-    }    @Override
+    }
+@Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Main.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("minerva.fxml"));
+        primaryStage.setTitle("Project Litera");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMinHeight(576);
+        primaryStage.setMinWidth(720);
+        primaryStage.show();
+
+        //DragMove support
+        root.setOnMousePressed(event -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+        //End of DragMove support
+
+        //Stage Closing event handler
+        primaryStage.setOnCloseRequest(we -> {
+            System.out.println("Stage is closing");
+            Controller.onExit();
+            System.out.println("Closed!");
+        });
+    }@Override
     public void start(Stage primaryStage) throws Exception
     {
         Main.primaryStage = primaryStage;
